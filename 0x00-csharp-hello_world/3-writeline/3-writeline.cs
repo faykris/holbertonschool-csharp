a5c3c2ce40bf9ad047ec0e1734c6135d2171c437
faykris <faykris28@gmail.com>
2022-11-20 21:19:04 -0500

    [Update] task 3 files - add @ before string

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,10 +1,1 @@
-﻿namespace WriteLine
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\"Programming is like building a multilingual puzzle");
-        }
-    }
-}
+﻿Console.WriteLine(@"""Programming is like building a multilingual puzzle");
